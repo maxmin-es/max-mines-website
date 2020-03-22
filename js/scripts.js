9c9b6b02df27ec0a468333c3c4b7ac1627756d05
maxmin-es <max_mines@brown.edu>
2020-03-21 22:51:10 -0400

    Fix safari compatibility

--- js/scripts.js
+++ js/scripts.js
@@ -4,38 +4,36 @@ $(document).ready(function() {
 	// projects fade
 	$("#projects").mouseover(function() {
 		$('.container').css({'background':'url(./img/maxdark.jpg) no-repeat center center fixed', 'background-size':'cover'})
-		$('.item-b').css({'opacity':'0%'});
-		$('.item-c').css({'opacity':'0%'});
+		$('.item-b').css({'opacity':'0'});
+		$('.item-c').css({'opacity':'0'});
 	});
 	$("#projects").mouseleave(function() {
 		$('.container').css({'background':bg, 'background-size':'cover'})
-		$('.item-b').css({'opacity':'80%'});
-		$('.item-c').css({'opacity':'100%'});
+		$('.item-b').css({'opacity':'.8'});
+		$('.item-c').css({'opacity':'1'});
 	});
 
 	// resume fade
 	$("#resume").mouseover(function() {
-		// bg = $('.container').css('background');
 		$('.container').css({'background':'url(./img/lp3.jpg) no-repeat center center fixed', 'background-size':'cover'})
-		$('.item-b').css({'opacity':'0%'});
-		$('.item-c').css({'opacity':'0%'});
+		$('.item-b').css({'opacity':'0'});
+		$('.item-c').css({'opacity':'0'});
 	});
 	$("#resume").mouseleave(function() {
 		$('.container').css({'background':bg, 'background-size':'cover'})
-		$('.item-b').css({'opacity':'80%'});
-		$('.item-c').css({'opacity':'100%'});
+		$('.item-b').css({'opacity':'.8'});
+		$('.item-c').css({'opacity':'1'});
 	});
 
 	// music fade
 	$("#music").mouseover(function() {
-		// bg = $('.container').css('background');
 		$('.container').css({'background': 'url(./img/lp2.jpg) no-repeat center center fixed', 'background-size':'cover'});
-		$('.item-b').css({'opacity':'0%'});
-		$('.item-c').css({'opacity':'0%'});
+		$('.item-b').css({'opacity':'0'});
+		$('.item-c').css({'opacity':'0'});
 	});
 	$("#music").mouseleave(function() {
 		$('.container').css({'background':bg, 'background-size':'cover'})
-		$('.item-b').css({'opacity':'80%'});
-		$('.item-c').css({'opacity':'100%'});
+		$('.item-b').css({'opacity':'.8'});
+		$('.item-c').css({'opacity':'1'});
 	});
 });
